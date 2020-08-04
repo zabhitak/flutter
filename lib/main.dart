005e0ce0b9b42d2ec0f934762ede99aa1858a5a8
zabhitak <zabhitak1292000@gmail.com>
2020-08-04 12:33:50 +0530

    list

---
+++ lib/main.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+import 'quote.dart';
+import 'quote_card.dart';
+
+void main() {
+  runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: QuoteList(),
+  ));
+}
+
+class QuoteList extends StatefulWidget {
+  @override
+  _QuoteListState createState() => _QuoteListState();
+}
+
+class _QuoteListState extends State<QuoteList> {
+  List<Quote> quotes = [
+    Quote(author: 'Abhinav', text: 'Hello'),
+    Quote(author: 'Abhinav2', text: 'Hello2'),
+    Quote(author: 'Abhinav3', text: 'Hello3'),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        backgroundColor: Colors.grey[200],
+        appBar: AppBar(
+          title: Text('Awesome Quotes'),
+          centerTitle: true,
+          backgroundColor: Colors.redAccent,
+        ),
+        body: Column(
+          children: quotes
+              // .map((quote) => Text('${quote.text} - ${quote.author}'))
+              .map((quote) => QuoteCard(
+                  quote: quote,
+                  delete: () {
+                    setState(() {
+                      quotes.remove(quote);
+                    });
+                  }))
+              .toList(),
+        ));
+  }
+}
